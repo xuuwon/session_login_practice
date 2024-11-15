d03cac3540dfe719a0b890ebae45b245186b3dc4
xuuwon <dumpling1011@gmail.com>
2024-11-15 14:48:23 +0900

    first commit

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const app = express();
 app.use(
   cors({
     // ⭐️ origin에 설정되어 있는 포트번호를 본인의 라이브서버 포트번호로 변경해주세요.
-    origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+    origin: ["http://127.0.0.1:5501", "http://localhost:5501"],
     methods: ["OPTIONS", "POST", "GET", "DELETE"],
     credentials: true,
   })
@@ -51,18 +51,22 @@ app.use(express.json());
 app.use(
   session({
     // 암호화, 열쇠 역할을 하는 문자열 설정
+    secret: 'session secret',
     // 요청이 들어왔을 때 변경되는 사항이 없는 경우 저장하지 않도록 설정
+    resave: false,
     // 요청이 들어왔을 때 내용이 비어있는 경우 저장하지 않도록 설정
+    saveUninitialized: false,
     // 쿠키 이름을 session_id로 변경
+    name: 'session_id'
   })
 );
 
 // POST 요청 (로그인 요청시 보내는 메소드)
 app.post("/", (req, res) => {
   // 2️⃣. 요청 바디에서 전달받은 값을 구조분해 할당을 사용하여 관리하세요.
-  const {} = req.body;
+  const {userId, userPassword} = req.body;
   // 3️⃣. (find 메서드를 사용하여) users의 정보와 사용자가 입력한 정보를 비교하여 일치하는 회원이 존재하는지 확인하는 로직을 작성하세요.
-  const userInfo = users.find();
+  const userInfo = users.find(el => el.user_id === userId && el.user_password === userPassword);
 
   if (!userInfo) {
     res.status(401).send("로그인 실패");
@@ -83,7 +87,9 @@ app.get("/", (req, res) => {
 // DELETE 요청
 app.delete("/", (req, res) => {
   // 4️⃣. 세션 내 정보를 삭제하는 메소드를 작성하세요.
+  req.session.destroy()
   // 5️⃣. 쿠키를 삭제하는 메소드를 작성하세요.
+  res.clearCookie('session_id')
   res.send("🧹세션 삭제 완료");
 });
 
